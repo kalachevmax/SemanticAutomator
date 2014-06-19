cccf44d5866443faac91326685f0b232be49ccfa
Maxim Kalachev <maxkalachev@yandex.ru>
2014-06-20 01:50:01 +0400

    application scheme

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 
 
-app = {};
+var app = {};
+var fs = require('fs');
+
+
+/**
+ * @type {string}
+ */
+app.SCHEME_FILE_NAME = 'scheme.json';
+
+
+/**
+ * @type {Object}
+ */
+app.__scheme = null;
 
 
 app.make = function() {
@@ -27,23 +40,46 @@ app.lint = function() {
 };
 
 
-app.usage = function() {
+function usage() {
   console.log('usage: app action');
   console.log('action = make|update|publish|check|compile|lint');
-};
+}
 
 
-app.handleInput = function() {
+function loadScheme(complete, cancel) {
+  fs.readFile(app.SCHEME_FILE_NAME, function(error, data) {
+    if (error) {
+      cancel('Scheme file reading error: ', error.toString());
+    }
+
+    try {
+      var scheme = JSON.parse(data);
+    } catch(error) {
+      cancel('Scheme file parsing error: ' + error.toString());
+    }
+
+    complete(scheme);
+  });
+}
+
+
+function handleInput() {
   if (process.argv.length > 2) {
     if (typeof app[process.argv[2]] === 'function') {
       app[process.argv[2]]();
     } else {
-      app.usage();
+      usage();
     }
   } else {
     app.make();
   }
-};
+}
 
 
-app.handleInput();
+function handleSchemeLoaded(scheme) {
+  app.__scheme = scheme;
+  handleInput();
+}
+
+
+loadScheme(handleSchemeLoaded, console.log);
