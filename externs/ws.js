50e3f4408ac193b5d1fe8671cda633faab5c5bb2
Max Kalachev <maxkalachev@yandex.ru>
2015-01-22 01:43:23 +0400

    ws externs

--- externs/ws.js
+++ externs/ws.js
@@ -3,63 +3,63 @@
 /**
  * @namespace
  */
-var ws = {};
+var wss = {};
 
 
 /**
  * @typedef {string|!Buffer}
  */
-ws.Body;
+wss.Message;
 
 
 /**
  * @param {!Object} options
  * @param {!Function=} connectionListener
  */
-ws.createServer = function (options, connectionListener) {};
+wss.createServer = function (options, connectionListener) {};
 
 
 /**
  * @param {string} address
  * @param {!Function=} openListener
  */
-ws.connect = function (address, openListener) {};
+wss.connect = function (address, openListener) {};
 
 
 /**
  * @constructor
  */
-ws.Client = function() {};
+wss.Client = function() {};
 
 
 /**
- * @param {ws.Body} data
+ * @param {wss.Message} data
  */
-ws.Client.prototype.send = function(data) {};
+wss.Client.prototype.send = function(data) {};
 
 
 /**
  * @param {string} event
  * @param {!Function} handler
  */
-ws.Client.prototype.on = function(event, handler) {};
+wss.Client.prototype.on = function(event, handler) {};
 
 
 /**
  * @constructor
  * @param {!Object} options
  */
-ws.Server = function(options) {};
+wss.Server = function(options) {};
 
 
 /**
  * @type {*}
  */
-ws.Server.prototype.clients;
+wss.Server.prototype.clients;
 
 
 /**
  * @param {string} event
  * @param {!Function} handler
  */
-ws.Server.prototype.on = function(event, handler) {};
+wss.Server.prototype.on = function(event, handler) {};
